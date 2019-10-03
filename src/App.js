bbe08ebe58d0d0f0c20a4308c23ea03c3447d23e
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-03 15:54:07 -0300

    using hooks, useState/useEffect/useMemo/useCallback

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return <div className="App" />;
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    /* evita que funções sejam recriadas toda vez que o state é alterado */
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+  /* agora a função só sera recriada quando as variaveis newTech ou tech forem alteradas */
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+    /* return () => {} , para componenteDidUnmount */
+  }, []);
+  /* para executar apenas uma unica vez, basta deixar sem uma dependencia  */
+
+  useEffect(() => {
+    /* recebe como primeiro parametro a função a ser executada */
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+  /* recebendo como segundo parametro quando vai ser executada */
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
